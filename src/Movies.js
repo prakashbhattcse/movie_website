fdde8551f73d76fdd196e408c526f58ab6918bec
Prakash Chandra Bhatt <77747508+prakashbhattcse@users.noreply.github.com>
2023-08-20 00:50:57 +0530

    movie page done

--- src/Movies.js
+++ src/Movies.js
@@ -12,11 +12,14 @@ const Movies = () => {
         <div className="container grid grid-4-col">
           {movie.map((curMovie) => {
             const { imdbID, Title, Poster } = curMovie;
+            const movieName = Title.substring(0, 15);
             return (
               <NavLink to={`movie/${imdbID}`} key={imdbID}>
                 <div className="card">
                   <div className="card-info">
-                    <h2>{Title}</h2>
+                    <h2>  {movieName.length > 13
+                      ? `${movieName}...`
+                      : movieName}</h2>
                     <img src={Poster} alt={imdbID} />
                   </div>
                 </div>
